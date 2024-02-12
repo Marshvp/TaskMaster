eeb0179b87b100e645c13ed3badee30e101593da
Marshall Powell <marshallpowell13@gmail.com>
2024-02-12 14:03:36 +0000

    Webpack basic setup

---
+++ dist/bundle 29ecc95a2cd33b15cc0b.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hello");
+/******/ })()
+;
+//# sourceMappingURL=bundle 29ecc95a2cd33b15cc0b.js.map
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name] [contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        
+                type: 'asset/resource',
+        
+              },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'TaskMaster',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
+}
